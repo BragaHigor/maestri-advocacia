c8599494e559d69bbb350c60ab1c2116305bf81d
BragaHigor <higorbraga.s@gmail.com>
2026-09-08 20:57:25 -0300

    feat: corricao logo secao fale com o advogado

--- src/components/sections/landing-sections.tsx
+++ src/components/sections/landing-sections.tsx
@@ -487,7 +487,7 @@ export function ContactSection() {
       id="contato"
     >
       <Image
-        className="pointer-events-none absolute bottom-[-20%] left-[-7%] h-auto w-[min(540px,54vw)] opacity-5"
+        className="pointer-events-none absolute bottom-[-20%] left-0 h-auto w-[min(540px,54vw)] opacity-5"
         src="/assets/brand/maestri-monograma-fundo-escuro.svg"
         alt=""
         width={1000}
